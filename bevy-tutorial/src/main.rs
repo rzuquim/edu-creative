8b35e9f4025430bb20e8cac57c335aa6858dd8da
Rafael Zuquim <rzuquim@gmail.com>
2025-01-23 23:12:17 -0300

    Bevy tutorial: ensuring movement run before confinement

--- bevy-tutorial/src/main.rs
+++ bevy-tutorial/src/main.rs
@@ -7,9 +7,16 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_camera)
         .add_plugins(player::Plugin)
+        .configure_sets(Update, MovementSystemSet.before(ConfinementSystemSet))
         .run();
 }
 
 pub fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
 }
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
--- bevy-tutorial/src/player/mod.rs
+++ bevy-tutorial/src/player/mod.rs
@@ -3,11 +3,13 @@ mod movement;
 
 use bevy::{
     app::{Startup, Update},
-    prelude::{App, Component},
+    prelude::{App, Component, IntoSystemConfigs},
 };
 
 use lifecycle::*;
 use movement::*;
+
+use super::{ConfinementSystemSet, MovementSystemSet};
 
 pub struct Plugin;
 
@@ -17,7 +19,8 @@ pub const PLAYER_SPRITE_SIZE: f32 = 64.0;
 impl bevy::app::Plugin for Plugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_player)
-            .add_systems(Update, (move_player, confine_player_movement));
+            .add_systems(Update, move_player.in_set(MovementSystemSet))
+            .add_systems(Update, confine_player_movement.in_set(ConfinementSystemSet));
     }
 }
 
