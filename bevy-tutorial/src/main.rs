459fd735387dd34b229a9da47f8bc0d212fa821f
Rafael Zuquim <rzuquim@gmail.com>
2025-01-25 18:04:37 -0300

    Bevy tutorial: setting up basic logging

--- bevy-tutorial/src/main.rs
+++ bevy-tutorial/src/main.rs
@@ -7,6 +7,8 @@ use bevy::prelude::{Camera2d, DefaultPlugins};
 use prelude::*;
 
 fn main() {
+    setup_settings();
+
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_camera)
@@ -15,12 +17,24 @@ fn main() {
         .run();
 }
 
+fn setup_settings() {
+    setup_logging();
+}
+
+fn setup_logging() {
+    if let Ok(_) = std::env::var("RUST_LOG") {
+        return;
+    }
+
+    let filter = if cfg!(debug_assertions) {
+        "warn,tutorial=debug"
+    } else {
+        "warn,tutorial=info"
+    };
+
+    std::env::set_var("RUST_LOG", filter);
+}
+
 pub fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
 }
-
-#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
-pub struct MovementSystemSet;
-
-#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
-pub struct ConfinementSystemSet;
--- bevy-tutorial/src/prelude.rs
+++ bevy-tutorial/src/prelude.rs
@@ -1,2 +1,8 @@
 pub use bevy::prelude::*;
 pub use bevy::window::PrimaryWindow;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
